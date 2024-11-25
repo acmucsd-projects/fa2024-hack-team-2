91fb364ecd61445591b5d18ff7c97f53daccda37
Chris <christopherrebollar0@icloud.com>
2024-11-24 17:09:21 -0800

    feat(pages, constants, components): Set up starter pages and file structure
    Created initial pages directories for the project: login, register, messages, profile, and homepage.
    Each route folder includes a `page.tsx` file and a corresponding `module.css` for styling.
    Additionally, a `constants/routes.ts` file was added to store the current routes for the project.
    
    In the app/ directory, a `components` folder was created to house the components for the homepage.
    
    Also, unnecessary files in the fonts and public folders were deleted to clean up the project.

---
+++ client/src/app/login/page.tsx
@@ -0,0 +1,11 @@
+import styles from './login.module.css';
+
+const LoginPage: React.FC = () => {
+    return (
+        <div className='title-demo'>
+            <h1>Login Page</h1>
+        </div>
+    )
+}
+
+export default LoginPage;
---
+++ client/src/app/register/page.tsx
@@ -0,0 +1,11 @@
+import styles from './register.module.css';
+
+const RegisterPage: React.FC = () => {
+    return (
+        <div className='title-demo'>
+            <h1>Register Page</h1>
+        </div>
+    )
+}
+
+export default RegisterPage;
