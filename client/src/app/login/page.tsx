c6f7705b8122362cec8247f35751c893bc11d8d6
OrigamiStarz <40199724+OrigamiStarz@users.noreply.github.com>
2024-12-11 12:01:16 -0800

    feat: Set up general layout

--- client/src/app/login/page.tsx
+++ client/src/app/login/page.tsx
@@ -1,11 +1,18 @@
-import styles from './login.module.css';
+import styles from "./login.module.css";
 
 const LoginPage: React.FC = () => {
-    return (
-        <div className='title-demo'>
-            <h1>Login Page</h1>
-        </div>
-    )
-}
+  return (
+    <div className="flex h-screen w-screen flex-col md:flex-row">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <div className="flex h-1/5 w-screen border-b-2 md:h-full md:w-2/5 md:border-b-0 md:border-r-2">
+        <p className="m-auto flex items-center text-center text-5xl font-bold text-blue-500">
+          Swipe Style
+        </p>
+      </div>
+      <div className="flex h-4/5 w-screen flex-col bg-blue-200 transition-all md:h-full md:w-3/5">
+      </div>
+    </div>
+  );
+};
 
 export default LoginPage;
