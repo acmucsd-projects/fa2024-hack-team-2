8d32d50ca57699406cc3fdf3e3438c88d096044e
theryanfo <ryan.s.fordham@gmail.com>
2024-12-16 20:58:51 -0800

    add imports, routes, and middleware for google OAuth with Passport and using express sessions

--- server/app.ts
+++ server/app.ts
@@ -3,9 +3,12 @@ import express, { Request, Response, NextFunction } from 'express';
 import path from 'path';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
+import passport from 'passport';
+import session from 'express-session';
 
 import indexRouter from './routes/index';
 import usersRouter from './routes/users';
+import authRouter from './routes/auth';
 
 import dotenv from 'dotenv';
 
@@ -24,9 +27,20 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(
+  session({
+    secret: 'sample-secret', 
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
 // Routes
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/auth', authRouter);
 
 // catch 404 and forward to error handler
 app.use((req: Request, res: Response, next: NextFunction) => {
