46fff5a8b1cfd16e662c14e300459272bd8a334f
OrigamiStarz <40199724+OrigamiStarz@users.noreply.github.com>
2024-12-16 23:46:56 -0800

    Feature/register-login-page (#30)
    * feat: create branch for register and login pages
    
    * feat: Set up general layout
    
    * feat: Style input fields
    
    * feat: Create login buttons for specific platforms
    
    * feat: Create password visibility toggle
    
    * feat: Create confirm password
    
    * feat: Make minor tweaks to buttons
    
    * feat: Implement password matching
    
    * style: Change quotes/indents with Prettier
    
    * feat: Implement form validation/routing
    
    * feat: Add components folder
    
    * fix: Add Tailwind config files
    
    * fix: Add value property to InputBox in /login
    
    * fix: Add value property to InputBox in /register

--- client/src/app/messages/page.tsx
+++ client/src/app/messages/page.tsx
@@ -1,11 +1,11 @@
-import styles from './messages.module.css';
+import styles from "./messages.module.css";
 
 const MessagesPage: React.FC = () => {
-    return (
-        <div className='title-demo'>
-            <h1>Messages Page</h1>
-        </div>
-    );
+  return (
+    <div className="title-demo">
+      <h1>Messages Page</h1>
+    </div>
+  );
 };
 
 export default MessagesPage;
--- client/src/app/profile/page.tsx
+++ client/src/app/profile/page.tsx
@@ -1,11 +1,11 @@
-import styles from './profile.module.css';
+import styles from "./profile.module.css";
 
 const ProfilePage: React.FC = () => {
-    return (
-        <div className='title-demo'>
-            <h1>Profile Page</h1>
-        </div>
-    );
+  return (
+    <div className="title-demo">
+      <h1>Profile Page</h1>
+    </div>
+  );
 };
 
 export default ProfilePage;
