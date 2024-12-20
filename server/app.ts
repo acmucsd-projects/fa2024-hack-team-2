d5d3bda2acbd13ad862ceab8704de8134e6fe914
Ryan Fordham <156359755+theryanfo@users.noreply.github.com>
2024-12-19 23:02:55 -0800

    Google OAuth (#39)
    * deleted node_modules
    
    * nothing
    
    * dotenv functionality
    
    * utils/passport.ts, create auth.ts, add routing to app.ts
    
    * routes/auth.ts successful log in test
    
    * changed session secret / added to .env
    
    * added protected route for testing

--- server/app.ts
+++ server/app.ts
@@ -3,9 +3,16 @@ import express, { Request, Response, NextFunction } from 'express';
 import path from 'path';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
+import passport from './utils/passport';
+import session from 'express-session';
 
 import indexRouter from './routes/index';
 import usersRouter from './routes/users';
+import authRouter from './routes/auth';
+
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 
@@ -20,9 +27,20 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET || "session-secret",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
 // Routes
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/auth', authRouter);
 
 // catch 404 and forward to error handler
 app.use((req: Request, res: Response, next: NextFunction) => {
