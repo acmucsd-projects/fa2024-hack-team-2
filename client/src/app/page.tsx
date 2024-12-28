bbae8dee69dabfb12e3d7895f3ed7d98e946ddc6
MacBook <macbook@MacBooks-MacBook-Pro.local>
2024-12-27 22:20:30 -0800

    middle title

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,10 +1,8 @@
 import styles from "./homepage.module.css";
-import ViewHistory from './History/ViewHistory';
 const Home: React.FC = () => {
   return (
     <main>
       <h1>Home Page</h1>
-      <ViewHistory />
 
     </main>
   );
