a9de52e02a929419a6361b3a002bda25dda88eef
watawada <k3mai@ucsd.edu>
2025-01-05 17:59:16 -0800

    Added user searching
    Allowed for user searching, which will be sorted by followers, and implemented sorting requests for the posts.

--- server/routes/search.js
+++ server/routes/search.js
@@ -14,18 +14,51 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const Post_1 = __importDefault(require("../models/Post"));
+const User_1 = require("../models/User");
 const levenshtein_edit_distance_1 = require("levenshtein-edit-distance");
 const router = express_1.default.Router();
-// GET: Find posts by name
+/**
+ * @route GET /posts
+ * @desc Search for a post
+ * @access Public
+ *
+ * Allows a user regardless of authorization to search for posts. The search feature
+ * has some degree of fuzziness to offer posts similar to the search. It has sorting
+ * and filtering depending on selected tags and relevance. The default sort is by likes (high to low).
+ *
+ * Request Query:
+ * - query: The name of the search.
+ * - tags: The desired tags to filter by.
+ * - relevance: The desired sorting of searches.
+ *
+ * Response:
+ * - 200: Search was successfully completed and retrieved posts.
+ * - 400: No post was found with the desired inputs.
+ * - 500: Internal server error.
+ */
 router.get('/posts', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     try {
         const query = req.query.query;
         const tags = req.query.tags ? req.query.tags.split(',') : null;
         const relevance = req.query.relevance;
+        const sortOptions = {
+            'priceHigh': { Price: -1 },
+            'priceLow': { Price: 1 },
+            'recent': { date: -1 },
+            'oldest': { date: 1 },
+            'likes': { likes: -1 }
+        };
+        let sortBy;
         let results = yield Post_1.default.find().sort({ likes: -1 });
         if (!query && !tags && !relevance) {
-            res.json(results);
+            res.status(200).json(results);
             return;
+        }
+        if (relevance && relevance in sortOptions) {
+            sortBy = sortOptions[relevance];
+        }
+        else {
+            sortBy = sortOptions['likes']; // If no specified relevance, then default to likes
         }
         if (tags) {
             results = yield Post_1.default.aggregate([
@@ -46,10 +79,9 @@ router.get('/posts', (req, res) => __awaiter(void 0, void 0, void 0, function* (
                     }
                 },
                 {
-                    $sort: {
-                        likes: -1,
-                        matchCount: -1
-                    }
+                    $sort: Object.assign(Object.assign({}, (relevance && relevance in sortOptions
+                        ? sortOptions[relevance]
+                        : { likes: -1 })), { matchCount: -1 })
                 },
                 {
                     $project: {
@@ -58,16 +90,58 @@ router.get('/posts', (req, res) => __awaiter(void 0, void 0, void 0, function* (
                 }
             ]);
         }
+        else {
+            results = yield Post_1.default.find().sort(sortBy);
+        }
         if (query) {
             results = results === null || results === void 0 ? void 0 : results.filter(post => {
                 return (0, levenshtein_edit_distance_1.levenshteinEditDistance)(query, post.title) <= 2;
             });
         }
         ;
-        res.json(results);
+        if (!results[0]) {
+            res.status(400).json({ message: 'No posts found' });
+            return;
+        }
+        res.status(200).json(results);
     }
     catch (err) {
-        res.status(500).json({ error: 'Error searching' });
+        res.status(500).json({ error: 'Error searching for posts' });
+    }
+}));
+/**
+ * @route GET /users
+ * @desc Search for users.
+ * @access Public
+ *
+ * Allows a user regardless of authorization to search for users by their username. Some degree of fuzziness
+ * is implemented. Users need to input '@' at the beginning to search for users instead of posts. If no search
+ * is specified, all users will be shown. The results are sorted by number of followers (high to low).
+ *
+ * Response:
+ * - 200: Search was successful.
+ * - 400: No user found.
+ * - 500: Internal server error.
+ */
+router.get('/users', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const allUsers = yield User_1.User.find().sort({ followers: -1 });
+        if (!req.query.query) {
+            res.status(200).json(allUsers);
+            return;
+        }
+        const query = req.query.query.replace('@', '').replace(/\s/g, "").toLowerCase();
+        const results = allUsers.filter(user => {
+            return (0, levenshtein_edit_distance_1.levenshteinEditDistance)(query, user.username.toLowerCase()) <= 3;
+        });
+        if (!results[0]) {
+            res.status(400).json({ message: `No user found: ${query}` });
+            return;
+        }
+        res.status(200).json(results);
+    }
+    catch (err) {
+        res.status(500).json({ error: 'Error searching for users' });
     }
 }));
 exports.default = router;
--- server/routes/search.ts
+++ server/routes/search.ts
@@ -1,20 +1,56 @@
 import express, {Request, Response} from 'express';
 import Post from '../models/Post';
+import { User } from '../models/User';
 import {levenshteinEditDistance} from 'levenshtein-edit-distance';
-
+import { SortOrder } from 'mongoose';
 const router = express.Router();
 
-// GET: Find posts by name
+/**
+ * @route GET /posts
+ * @desc Search for a post
+ * @access Public
+ * 
+ * Allows a user regardless of authorization to search for posts. The search feature 
+ * has some degree of fuzziness to offer posts similar to the search. It has sorting
+ * and filtering depending on selected tags and relevance. The default sort is by likes (high to low).
+ * 
+ * Request Query:
+ * - query: The name of the search.
+ * - tags: The desired tags to filter by.
+ * - relevance: The desired sorting of searches.
+ * 
+ * Response:
+ * - 200: Search was successfully completed and retrieved posts.
+ * - 400: No post was found with the desired inputs.
+ * - 500: Internal server error.
+ */
+
 router.get('/posts', async (req, res) => {
     try {
         const query = req.query.query;
         const tags = req.query.tags ? (req.query.tags as string).split(','):null;
-        const relevance = req.query.relevance;
+        const relevance = req.query.relevance as string;
+
+        const sortOptions: {[key: string]: {[key:string]: SortOrder}} = {
+            'priceHigh': {Price: -1},
+            'priceLow': {Price: 1},
+            'recent': {date: -1},
+            'oldest': {date: 1},
+            'likes': {likes: -1}
+        };
+
+        let sortBy;
         let results = await Post.find().sort({likes: -1});
         
         if (!query && !tags && !relevance){
-            res.json(results);
+            res.status(200).json(results);
             return;
+        }
+
+        if (relevance && relevance as string in sortOptions){
+            sortBy = sortOptions[relevance];
+        } else{
+            sortBy = sortOptions['likes'] // If no specified relevance, then default to likes
         }
         
         if(tags){
@@ -37,7 +73,10 @@ router.get('/posts', async (req, res) => {
                 },
                 {
                     $sort:{
-                        likes: -1,
+                        ...(relevance && relevance in sortOptions
+                            ? sortOptions[relevance]
+                            : {likes: -1}
+                        ),
                         matchCount: -1,
                     }
                 },
@@ -47,6 +86,8 @@ router.get('/posts', async (req, res) => {
                     }
                 }
             ]);
+        } else {
+            results = await Post.find().sort(sortBy);
         }
 
         if (query){
@@ -55,11 +96,54 @@ router.get('/posts', async (req, res) => {
             });
         };
         
+        if(!results[0]){
+            res.status(400).json({message: 'No posts found'});
+            return;
+        }
 
-        res.json(results);
+        res.status(200).json(results);
     } catch (err){
-        res.status(500).json({ error: 'Error searching' });
+        res.status(500).json({ error: 'Error searching for posts' });
     }
 })
 
+/**
+ * @route GET /users
+ * @desc Search for users.
+ * @access Public
+ * 
+ * Allows a user regardless of authorization to search for users by their username. Some degree of fuzziness
+ * is implemented. Users need to input '@' at the beginning to search for users instead of posts. If no search
+ * is specified, all users will be shown. The results are sorted by number of followers (high to low).
+ * 
+ * Response:
+ * - 200: Search was successful.
+ * - 400: No user found.
+ * - 500: Internal server error.
+ */
+
+router.get('/users', async(req, res) => {
+    try {
+  
+        const allUsers = await User.find().sort({followers: -1});
+
+        if(!req.query.query){
+            res.status(200).json(allUsers);
+            return;
+        }
+        const query = (req.query.query as string).replace('@', '').replace(/\s/g, "").toLowerCase();
+
+        const results = allUsers.filter(user => {
+            return levenshteinEditDistance(query, user.username.toLowerCase()) <= 3;
+        })
+
+        if (!results[0]){
+            res.status(400).json({message: `No user found: ${query}`});
+            return;
+        }
+        res.status(200).json(results);
+    } catch(err){
+        res.status(500).json({error: 'Error searching for users'})
+    }
+})
 export default router;
